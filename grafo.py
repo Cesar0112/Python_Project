cbe298cf7b28b8bb59558f6c26ecc28141e2ec5a
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-05-24 18:07:34 -0400

    Los metodo retornan el mensaje correspondiente a lo que hacen

--- grafo.py
+++ grafo.py
@@ -9,31 +9,38 @@ con sus funciones de control y creacion """
 grafo = {}
 
 
-def insertarNodo(etiqueta: str):
+def insertarNodo(etiqueta: str) -> str:
     """#verificar si ya existe para poder introducirlo"""
     if etiqueta not in grafo:  # si el nodo existe(llave)
         grafo[etiqueta] = []
+        return "Nodo insertado con exito" #insertado con exito
+    else:
+        return "ERROR: Ese nodo ya existe en el grafo" #no insertado con exito
 
-
-def insertarArista(nodoInicial: str, nodoFinal: str, peso):
-    """#verificar si existe una arista entre esos nodos con el mismo peso"""
+def insertarArista(nodoInicial: str, nodoFinal: str, peso) -> str:
+    """#verificar si existe una arista entre esos nodos con el mismo peso
+    si devuelve:
+    """
     # si es asi no se inserta esa arista
 
     if nodoInicial in grafo: #si existe el nodo inicial
         if nodoFinal in grafo: #si existe el nodo final
             if [nodoFinal, peso] not in grafo[nodoInicial]: #si ese camino (arista) no existe entonces se agrega de lo contrario
-                grafo[nodoInicial].append([nodoFinal, peso])#agregale la lista
+                grafo[nodoInicial].append([nodoFinal, peso])#agregale a la lista
+                return "Arista insertada correctamente"
             else:
-                print("ERROR: ya existe esa arista en el grafo")
+                return "ERROR: Ya existe una arista entre esos nodos con ese peso"
         else:
-            print("ERROR: No existe el nodo Final")
+            return "ERROR: No existe el nodo final"
     else:
-        print("ERROR: No existe el nodo Inicial")
+        return "ERROR: No existe el nodo inicial"
 
-def eliminarNodo(etiqueta: str):
-    """#verificar si existe antes de intentar borrarlo"""
+def eliminarNodo(etiqueta: str) -> str:
+    """#verificar si existe antes de intentar borrarlo
+    retorna el nodo eliminado
+    """
     if etiqueta in grafo:
-        grafo.pop(etiqueta)#elimina el elemento en la parte de las claves
+        nodoEliminado = grafo.pop(etiqueta)#elimina el elemento en la parte de las claves
 
 
         #eliminar todas las incidencias en todos lo nodos
@@ -41,22 +48,27 @@ def eliminarNodo(etiqueta: str):
             for lista in listaDeListas:#recorre cada lista dentro de la lista de listas
                 if etiqueta in lista[0]:#si encuentra la etiqueta en la parte de etiqueta de la lista eliminalo
                     listaDeListas.remove(lista)#elimina la lista que contiene la etiqueta del nodo
+
+        return "Nodo "+nodoEliminado+" eliminado correctamente"
     else:
-        print("ERROR: Esa etiqueta no existe")
+        return "ERROR: No existe el nodo indicado"
 
 
-def eliminarArista(nodoInicial: str, nodoFinal: str, peso):
-    """#verificar si existe antes de intentar borrarla"""
+
+def eliminarArista(nodoInicial: str, nodoFinal: str, peso) -> str:
+    """#verificar si existe antes de intentar borrarla
+    """
     if (nodoInicial in grafo) and (nodoFinal in grafo):
         if [nodoFinal, peso] in grafo[nodoInicial]: #si encuentra la conexion al nodo final desde el inicial con el mismo peso
             (grafo[nodoInicial]).remove([nodoFinal, peso])
+            return "Eliminación exitosa"
         else:
-            print("ERROR:No existe esa arista")
+            return "ERROR: No existe esa arista"
     else:
-        print("ERROR:Esa arista no existe porque los nodos indicados son incorrectos")
+        return "ERROR: No existe esa arista entre los nodos indicados"
 
 
-def modificarNodo(etiquetaActual: str, etiquetaNueva: str):
+def modificarNodo(etiquetaActual: str, etiquetaNueva: str) -> str:
     """verificar si existe antes de intentar modificar"""
 
     if etiquetaActual in grafo:#si existe el nodo entonces se puede modificar
@@ -69,10 +81,11 @@ def modificarNodo(etiquetaActual: str, etiquetaNueva: str):
                 for lista in listaDeListas:#recorre las listas(arista) de cada lista de listas
                     if etiquetaActual in lista[0]:#si encuentra la referencia al nodo que se desea modificar se cambia
                         lista[0] = etiquetaNueva#se cambia la etiqueta de esa referencia
+            return "Nodo modificado"
         else:
-            print("ERROR:Ya existe ese nodo")
+            return "ERROR:Ya existe ese nodo en el grafo"
     else:
-        print("ERROR: No exite ese nodo en el grafo")
+        return "ERROR: No existe el nodo a modificar, en el grafo"
 
 def modificarArista(nodoInicial: str, nodoFinal: str, pesoActual, pesoNuevo):
     """#verificar su existencia antes de modificar la arista"""
@@ -86,32 +99,32 @@ def modificarArista(nodoInicial: str, nodoFinal: str, pesoActual, pesoNuevo):
                         lista[1] = pesoNuevo #cambia el peso actual al peso nuevo
                         encontroArista = True
                     else:
-                        print(f"ERROR:No existe arista entre esos nodos con el peso {pesoActual}")
+                        return f"ERROR:No existe arista entre esos nodos con el peso {pesoActual}"
                 else:#si no encuentra conexion significa que no existe arista entre esos nodos con direccion al nodo final
                     if not encontroArista:
-                        print(f"ERROR:No existe arista de  {nodoInicial}->{nodoFinal}")
+                        return f"ERROR:No existe arista de  {nodoInicial}->{nodoFinal}"
         else:
-            print("ERROR:No existe arista con el nodo final indicado")
+            return "ERROR:No existe arista con el nodo final indicado"
     else:
-        print("ERROR:No existe arista con el nodo inicial indicado")
+        return "ERROR:No existe arista con el nodo inicial indicado"
 
 
-def importarGrafo(nombreArchivo: str) :
+def importarGrafo(nombreArchivo: str) -> str:
     """ Permite importar un grafo desde un txt """
-    grafo={}
+
     with open(nombreArchivo, 'r', encoding='utf8') as archivo:
         for lineaTexto in archivo:
-            linea = lineaTexto.strip().split(';')
-            if linea[0] not in grafo:
-                grafo[linea[0]] = []  # declaro el espacio de la comuna como un arreglo para almacenar listas en este
-            if len(linea) > 1:#si hay aristas que salen de ese nodo
+            linea = lineaTexto.strip().split(';')#divido la linea leida en un arreglo dividido por punto y comas
+            if linea[0] not in grafo:#verificar que el nodo  no este en el grafo
+                grafo[linea[0]] = []  # declaro el espacio del nodo como una lista para almacenar listas de solo dos elementos en este
+            if len(linea) > 1:#si hay aristas que salen de ese nodo hacia otro aunque sea a el
                 for arista in linea[1:]:#recorre cada arista
                     grafo[linea[0]].append(arista.strip().split(','))#agregala al grafo al nodo que le corresponde
 
-    return grafo
+    return "Grafo importado correctamente"
 
 
-def exportarGrafo(nombreArchivo:str,grafo: dict):
+def exportarGrafo(nombreArchivo:str,grafo: dict) -> str :
     """ Permite exportar un grafo a un archivo txt """
     archivo =open(nombreArchivo,"w")
     for nodo, listaDeListas in grafo.items():
@@ -120,16 +133,17 @@ def exportarGrafo(nombreArchivo:str,grafo: dict):
         if len(listaDeListas) > 1:#si hay mas de 1 elemento significa que el nodo clave se conecta a otros nodos
             for lista in listaDeListas:
                 linea += ";"+str(lista)
+                #remplazar todos los caracteres que no sea comas ni punto y comas con caracter vacio
                 linea = linea.replace("[","").replace("]","").replace("'","").replace(" ","")
             linea += "\n"
         archivo.write(linea)
     archivo.close()
+    return "Grafo exportado correctamente"
 
 def recorrerProfundidad() -> list:
     """ Recorre el grafo a lo profundo """
     recorrido = []
     pila=[]
-    ##TODO
     nodoInicial=0
 
     #se escoje el nodo inicial
@@ -159,7 +173,6 @@ def recorrerAncho() -> list:
     """ Recorre el grafo a lo ancho """
     recorrido = []
     cola=[]
-    ##TODO
     nodoInicial=0
 
     #se escoje el nodo inicial
